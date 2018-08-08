9531057a66c3c1484962ccf78306e805bb8b92c0
anggerpdsw <42221379+anggerpdsw@users.noreply.github.com>
2018-08-09 04:00:30 +0700

    Create index.php

---
+++ theme/sdk-tpb/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+         <title>Tema Wp pertama</title>
+    </head>
+
+<body>
+    <h1>Hello world, ini adalah tema Wordpress perama saya</h1>
+    <p>dibuat dari nol dengan penuh semangat<p>
+</body>
+<html>
